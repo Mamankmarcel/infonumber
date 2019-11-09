0558300c64520ce25d4ff257fbc860c03787ffa9
Mamankmarcel <57529908+Mamankmarcel@users.noreply.github.com>
2019-11-09 13:07:05 +0700

    Add files via upload
    Instalation
    $ pkg install git
    $ git clone https://github.com/mamankmarcel/infonumber.git
    $ cd infonumber
    $ chmod +x * info.sh
    $. /info.sh

---
+++ info.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+clear
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+bln="\033[33m"
+bc="\033[0m" 
+echo "$red ==============="
+echo "$red get info number"
+echo "$red ==============="
+echo "$yellow v1"
+sleep 1
+echo "memuat.. "
+sleep 1
+read -p "$green nomor: " nomor
+echo 
+echo "memuat info nomor $nomor.. "
+echo
+sleep 1
+echo "gagal bangsd"
+sleep 1
+echo "gagal bangsd"
+sleep 5
+exit
